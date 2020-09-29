d66b170a95038a7975ccbb3fcbbf97bb3478186a
SatheeshkumarG215 <69619117+SatheeshkumarG215@users.noreply.github.com>
2020-09-29 17:19:53 +0530

    Initial commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("coRoutines");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator coRoutines()
+    {
+        Debug.Log("Started");
+        yield return new WaitForSeconds(4f);
+        Debug.Log("Ended");
+    }
+}
